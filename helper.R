67052d0f12ec589c4021da117ad8da64a1dbf85e
Gregor Sturm <mail@gregor-sturm.de>
2016-11-02 10:06:51 +0100

    Update README, fix small issues in case studies.
    Fix mistakes in bioqc-wmw-test-performance.

--- helper.R
+++ helper.R
@@ -9,7 +9,7 @@
 ## Source: http://www.cookbook-r.com/Manipulating_data/Summarizing_data/
 summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
         conf.interval=.95, .drop=TRUE) {
-    library(plyr)
+    suppressPackageStartupMessages(library(plyr))
 
 # New version of length which can handle NA's: if na.rm==T, don't count them
         length2 <- function (x, na.rm=FALSE) {
